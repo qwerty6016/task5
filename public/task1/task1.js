57e92f100e5c1b5a19f25f360669410332f719f7
qwerty6016 <emailhere>
2019-05-24 21:48:33 +0300

    fixed float result in task1

--- public/task1/task1.js
+++ public/task1/task1.js
@@ -3,7 +3,7 @@
 const sum = function sumFirstNumberAndSecondNumber() {
   const firstNumber = parseFloat(document.getElementById('firstNumber').value);
   const secondNumber = parseFloat(document.getElementById('secondNumber').value);
-  const result = firstNumber + secondNumber;
+  const result = (firstNumber * 10 + secondNumber * 10) / 10;
 
   alert(`Результат:\n ${result}`);
 }
