81bd79644ac2b7fef0d139c279edace2629e3c0c
qwerty6016 <emailhere>
2020-01-11 02:24:05 +0300

    added questionnaire

--- index.js
+++ index.js
@@ -49,6 +49,9 @@ router
   .get('/registration', async ctx => {
     await send(ctx, '/public/registration/registration.html');
   })
+  .get('/questionnaire', async ctx => {
+    await send(ctx, '/public/questionnaire/questionnaire.html');
+  })
   .get('/users', async ctx => {
     await Promise.resolve(users.getUsers(db))
       .then(result => {
